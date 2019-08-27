7b1a72ea658e71f1871501949a7ad0325a848768
David Fisher <fisherds@rose-hulman.edu>
2019-08-27 16:07:00 -0400

    created a second template folder
    removed the login portions of the template

---
+++ basic/public/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview
+ * Provides interactions for all pages in the UI.
+ *
+ * @author 
+ */
+
+/** namespace. */
+var rh = rh || {};
+rh.appname = rh.appname || {};
+
+/** globals */
+rh.appname.variableName = "";
+
+rh.appname.functionName = function () {
+	/** function body */
+};
+
+rh.appname.ClassName = function () {
+	/** constructor */
+};
+
+rh.appname.ClassName.prototype.methodName = function (argsIfNeeded) {
+	/** class method */
+};
+
+rh.appname.sharedInit = function () {
+    console.log("TODO: Initialize click listeners, etc.");
+};
+
+
+
+/* Main */
+$(document).ready(function () {
+    rh.appname.sharedInit();
+});
