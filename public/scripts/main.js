ade06cb8a88fc43990f3f8a43ea2134d3b005922
rhit-dharman <dharman@rose-hulman.edu>
2022-12-05 21:38:14 -0500

    frameworks done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,24 +6,33 @@
  * Nikhil Dharmavaram
  */
 
-
+var rhit = rhit || {};
 rhit.counter = 0;
 
 
-rhit.updateCounter = function () {
-
+rhit.updateCounter = function (amount, isMultiplication) {
+	if (isMultiplication) {
+		rhit.counter *= amount;
+	}
+	else {
+		rhit.counter += amount;
+	}
+	document.querySelector("#counterText").innerHTML = `Count = ${rhit.counter}`;
+	
 };
 
 
 rhit.main = function () {
 	console.log("Ready");
 	const buttons = document.querySelectorAll("#counterButtons button");
-	buttons.forEach((button) => {
-		button.onclick = (event) => {
-			const dataAmount = button.dataset.amount;
-			const dataIsMultiplication = button.dataset.isMultiplication;
-			console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
-	});
+	for (const button of buttons){
+	 	button.onclick = (event) => {
+	 		const dataAmount = parseInt(button.dataset.amount);
+	 		const dataIsMultiplication = button.dataset.isMultiplication == "true";
+	 		rhit.updateCounter(dataAmount, dataIsMultiplication);
+	 	}
+	 }
+	
 };
 
 rhit.main();
